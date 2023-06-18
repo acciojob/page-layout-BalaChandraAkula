9bffddb22a1493e838c835e811fd0e1f919c84e4
BalaChandraAkula <balachandraakula698@gmail.com>
2023-06-18 12:15:04 +0530

    submitting code-Sun Jun 18 2023 12:15:01 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,25 @@
 
 import React from "react";
 import './../styles/App.css';
+import Header from './Header'
+import Content from './Content'
+import Footer from './Footer'
+
 
 const App = () => {
+  let x = "Welcome to my website"
+  let y = "This is the content of my website."
+  let z = "©️ 2023 my Website. All rights reserved."
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <>
+      {/* hello */}
+        {/* Do not remove the main  */}
+        <Header header={x}/>
+        <Content content={y}/>
+        {/* <Footer footer={z}/> */}
+        {/* <p className="footer">©️ 2023 my Website. All rights reserved.</p> */}
+        © 2023 My Website. All rights reserved.
+    </>
   )
 }
 
